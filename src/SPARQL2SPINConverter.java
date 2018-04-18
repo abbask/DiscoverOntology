fcb4e4e34e0c9d1538aabf4d86055f6e26e7de80
Abbas <abbas.keshavarzi@gmail.com>
2018-04-18 17:29:36 -0400

    reverted the Sparql2Spin to simple query

--- src/SPARQL2SPINConverter.java
+++ src/SPARQL2SPINConverter.java
@@ -26,25 +26,26 @@ public class SPARQL2SPINConverter
 		// Create an empty OntModel
 		final Model model = ModelFactory.createDefaultModel();
 		
-		//String query = "select distinct ?Concept where {[] a ?Concept} LIMIT 100";
-		String query = "PREFIX ta:      <https://tutorial-academy.com/2015/spin#> PREFIX sp:      <http://spinrdf.org/sp#>  PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#> SELECT ( ( AVG ( ?value ) ) AS ?average ) ?sensor WHERE { 	?measurement 	rdf:type 		ta:Measurement .     ?measurement 	ta:timestamp 	?timestamp .     ?measurement ta:value 		?value .     ?measurement 	ta:sensor 		?sensor . } GROUP BY ( ?sensor ) ORDER BY DESC ( ?average ) ";
+		String query = "select distinct ?Concept where {[] a ?Concept} LIMIT 100";
+		//String query = "PREFIX ta:      <https://tutorial-academy.com/2015/spin#> PREFIX sp:      <http://spinrdf.org/sp#>  PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#> SELECT ( ( AVG ( ?value ) ) AS ?average ) ?sensor WHERE { 	?measurement 	rdf:type 		ta:Measurement .     ?measurement 	ta:timestamp 	?timestamp .     ?measurement ta:value 		?value .     ?measurement 	ta:sensor 		?sensor . } GROUP BY ( ?sensor ) ORDER BY DESC ( ?average ) ";
 					
 		Query arqQuery = ARQFactory.get().createQuery( model, query );
 		ARQ2SPIN arq2SPIN = new ARQ2SPIN( model );
+		
 		Select sparqlQuery = (Select) arq2SPIN.createQuery( arqQuery, null );
 		
 		System.out.println( "SPARQL Query:\n" + sparqlQuery );
-		System.out.println( "\nSPIN Representation:" );
+		
 		System.out.println( "\nSPIN Representation:" );
 		model.write( System.out, FileUtils.langTurtle );
 		
-		String fileNameRDF = "result-complex.rdf";
-		String fileNameOWL = "result-complex.owl";
-		FileWriter outRDF ;
+		//String fileNameRDF = "result-simple.rdf";
+		String fileNameOWL = "result-simple.owl";
+		//FileWriter outRDF ;
 		FileWriter outOWL ;
 		try {
-			outRDF = new FileWriter( fileNameRDF );
-			model.write(outRDF, "RDF/XML");
+			//outRDF = new FileWriter( fileNameRDF );
+			//model.write(outRDF, "RDF/XML");
 			
 			outOWL = new FileWriter( fileNameOWL );
 			model.write(outOWL, "RDF/XML-ABBREV");
