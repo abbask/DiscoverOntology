413ff1b903be597e6d0efffb64400cbc4aede2f5
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2018-07-12 18:16:57 +0430

    set timezone in jdbc

--- src/edu/uga/discoverontology/datastore/MySQLConnection.java
+++ src/edu/uga/discoverontology/datastore/MySQLConnection.java
@@ -15,7 +15,7 @@ public class MySQLConnection {
 	
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 	//static final String JDBC_DB_URL = "jdbc:mysql://localhost:3306/discover_ontology?useUnicode=yes&characterEncoding=UTF-8"; //http://localhost/phpmyadmin/
-	static final String JDBC_DB_URL = "jdbc:mysql://localhost:3306/discover_ontology"; //http://localhost/phpmyadmin/
+	static final String JDBC_DB_URL = "jdbc:mysql://localhost:3306/discover_ontology?serverTimezone=UTC"; //http://localhost/phpmyadmin/
 	
 	static final String JDBC_USER = "root";
 	static final String JDBC_PASS = "root";
